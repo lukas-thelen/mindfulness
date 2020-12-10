649be886d4784014a71a127913e54a90ed3188d3
Blumbi <65356970+Blumbi@users.noreply.github.com>
2020-12-10 16:21:56 +0100

    asd
    asd

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
-
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
   return (
     <View style={styles.container}>
-      <Text> Mindfulness against addiction! </Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
