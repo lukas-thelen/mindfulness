2cd87ec692af8fd41d161257dd6c120b313af3ce
Blumbi <65356970+Blumbi@users.noreply.github.com>
2020-12-22 20:10:57 +0100

    Markus Session 22.12.
    Also ich hab folgendes geschafft:
    - Der Bug ist gefixt, also es funktioniert alles direkt beim ersten mal drücken. Ich verwende dafür in deiner
    - ich hab ein paar Stunden versucht einen Slider einuzubinden aber hat leider nicht funktioniert, ich hab dann einfach vorerst radiobuttons verwendet
    - Stressscale zählt jetzt hoch (jedesmal wenn man auf weiter drückt wird eine funktion getriggert die calculated)
    - Den kalkulierte Stresswert ist in der Variable "calculatedStressLevel" gespeichert, die ausgegeben wird wenn man auf Zeige Daten klickt.

--- src/StressSkala.js
+++ src/StressSkala.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import { Button, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RadioButtonRN from 'radio-buttons-react-native';
+import Slider from '@react-native-community/slider';
 
 import {AppContext} from './context.js';
 import { useContext } from 'react';
+import { abs } from 'react-native-reanimated';
 
 export const StressSkala = (props) =>{
     const [stressData, changeStressData] = useState("")
@@ -16,9 +18,9 @@ export const StressSkala = (props) =>{
                     "Wie oft hast DU dich im letzten Monat nervös und gestresst gefühlt?",
                     "Wie oft warst Du im letzten Monat zuversichtlich, dass Du fähig bist, ihre persönlichen Probleme zu ubewältigen?",
                     "Wie oft hast Du im letzten Monat das Gefühl, dass sich die Dinge zu Ihren Gunsten entwickeln?",
-                    "Wie oft hattest Du im letzten Monat den Eindruck, nicht all Deine anstehenden Aufgaben gewachsen zu sein?",
+                    "Wie oft hattest Du im letzten Monat den Eindruck, nicht all Deinen anstehenden Aufgaben gewachsen zu sein?",
                     "Wie oft warst Du im letzten Monat in der Lage, ärgerliche Situationen in Deinem Leben zu beeinflussen?",
-                    "Wie oft hastt Du im letzten Monat das Gefühl, alles im Griff zu haben?",
+                    "Wie oft hast Du im letzten Monat das Gefühl, alles im Griff zu haben?",
                     "Wie oft hast Du dich im letzten Monat über Dinge gegeärgert, über die Du keine Kontrolle hattest?",
                     "Wie oft hattest Du im letzten Monat das Gefühl, dass sich so viele Schwierigkeiten angehäufthaben, dass Du diese nicht überwinden konntest?",
                   ]
@@ -26,6 +28,9 @@ export const StressSkala = (props) =>{
       // Variablen für den dynamischen Button (von "Nächste Frage" --> Abschicken)
       const [question, changeQuestion] = useState(questions[number])
       const [buttonValue, changeButtonValue] = useState("Nächste Frage")
+
+      //Variable um Stresslevel zu kalkulieren
+      const [calculatedStressLevel,addStress] = useState(0)
 
 
     // Nutzerinfos im AsyncStorage speichern
@@ -35,18 +40,50 @@ export const StressSkala = (props) =>{
       props.changeProgressData(progressData)
     }
 
+    const test = () =>{
+      console.log("Stressdata",stressData)
+      console.log("calculatedStressLevel",calculatedStressLevel)
+      console.log("number",number)
+      
+  }
 
+  
     //Nutzereingabe prüfen
     const checkInput =()=>{
         if (stressData === ""){
           /*Alert.alert(
             'Unvollständig',
-            'Bitte wähle dein Level!',
+            'Bitte wähle dein Levell!',
             [{ text: 'OK'}],
             { cancelable: false }
             ); */
       }
     }
+    // Stress-Level angeben
+    const skala = ["Nie","Fast nie","Manchmal","Ziemlich oft","Sehr oft"]
+    const stressLevel = [
+      {label: skala[0]},{label: skala[1]},{label: skala[2]},{label: skala[3]},{label: skala[4]}];
+
+
+    //Kalkulation des Stresslevels    
+    const calculateStress =()=>{
+      if (buttonValue != "Abschicken"){
+          if (number === 0 || number === 1 || number === 2 || number === 5 || number === 8 || number === 9){
+                console.log("Erste if mit number= ",number)
+                const currentStress = calculatedStressLevel + skala.indexOf(stressData)+1;
+                addStress(currentStress);
+                console.log("Currentstress: ",currentStress)
+              } else{
+              if (number === 3 || number === 4 || number === 6 || number === 7){
+                console.log("Zweite if mit number= ",number)
+                const currentStress = calculatedStressLevel + (skala.indexOf(stressData)-5)*(-1);
+                addStress(currentStress);
+                console.log("Currentstress: ",currentStress)
+              }
+          }
+        }
+      }
+
     
     // Setzt die nächste Frage, sollte es die letzte Frage in questions sein, beendet sie die Umfrage und beendet Init
     const weiter = () =>{
@@ -55,8 +92,9 @@ export const StressSkala = (props) =>{
         } else {
           const currentNumber = number +1
           changeNumber(currentNumber)
-          changeQuestion(questions[number])
-            if (number === 9){
+          const currentQuestion = currentNumber
+          changeQuestion(questions[currentQuestion])
+            if (number === 8){
               changeButtonValue("Abschicken")
         }
       }
@@ -66,9 +104,14 @@ export const StressSkala = (props) =>{
         <View style={styles.pagewrap, styles.container}>
             <Text>Lass uns ein Stress-Tagebuch führen</Text>
             <Text>{question}</Text>
+            <RadioButtonRN
+              boxStyle={styles.radio}
+              data={stressLevel}
+              selectedBtn={(e) => changeStressData(e.label)}
+            />
             <Button title={"zeige Daten"} onPress={() =>{test()}} ></Button>
-            <Button title={buttonValue} onPress={() =>{weiter()}} ></Button>
-            <Button title={"Abschicken"} onPress={() =>{abschicken()}} ></Button>
+            <Button title={buttonValue} onPress={() =>{calculateStress(),weiter()}} ></Button>
+
         </View>
     )
 
