ee2ba93088007ede5944d0d5f54e2406501f2b2f
JASIN AZIZI <JASIN.AZIZI@stud.uni-due.de>
2020-12-22 21:38:47 +0100

    im async storage gespeichert ob die uebung bereits abgespielt wurde

--- src/HomeStack/AudioPlayer.js
+++ src/HomeStack/AudioPlayer.js
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
 import { Audio } from 'expo-av';
-import {AppContext} from './context.js';
-import { useContext } from 'react';
 
 import {kurse} from "../Kursdaten/Kursdatei.js"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AudioPlayer =({navigation, route})=>{
     const kurs=route.params.kursIndex
@@ -26,6 +25,17 @@ export const AudioPlayer =({navigation, route})=>{
         }
         
     play()
+
+    async function saveUebung() {
+        try {
+            await AsyncStorage.removeItem(JSON.stringify(kurse[kurs].Uebungen[uebung]))
+        } finally {
+            const jsonValue = JSON.stringify(kurse[kurs].Uebungen[uebung])
+            await AsyncStorage.setItem(jsonValue, 'true')
+        }
+    }
+
+    saveUebung()
 
     return(
         <View>
