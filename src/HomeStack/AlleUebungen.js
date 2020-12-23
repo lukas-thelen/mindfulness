dfc0d081c524f41bbed048f66a308e6961e15d00
JASIN AZIZI <JASIN.AZIZI@stud.uni-due.de>
2020-12-23 01:58:13 +0100

    Text wird grau nach abgespielter Übung

--- src/HomeStack/AlleUebungen.js
+++ src/HomeStack/AlleUebungen.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import {AppContext} from "../context.js"; 
+import { useContext } from 'react';
 import {kurse} from "../Kursdaten/Kursdatei.js"
 import {uebungen} from "../Kursdaten/Uebungsliste.js"
 
@@ -9,16 +11,18 @@ export const AlleUebungen=({navigation})=>{
     const renderItem =({item})=>{
         return(
             <TouchableOpacity style={styles.KursItem} onPress={()=>{navigation.navigate("Wähle eine Version", {kursIndex:item.KursIndex, uebung:item.id })}}>
-                <Text>{item.Name}</Text>
-                <Text>Kurs: {kurse[item.KursIndex].Name}</Text>
+                {uebungBereitsGehoert(item.id) ? <Text style={{color: "grey"}}>Kurs: {item.Name}</Text> : <Text >Kurs: {item.Name}</Text>}
+                {uebungBereitsGehoert(item.id) ? <Text style={{color: "grey"}}>Kurs: {kurse[item.KursIndex].Name}</Text> : <Text >Kurs: {kurse[item.KursIndex].Name}</Text>}
             </TouchableOpacity>
         )
     }
     const atemuebungen = uebungen.filter(item=>item.Kategorie==="Atemübung")
     const mindfulness = uebungen.filter(item=>item.Kategorie==="Mindfulness")
+    const gehoerteUebungen = useContext(AppContext).gehoerteUebungen;
 
     function uebungBereitsGehoert (uebung_id) {
-        return (uebung_id.indexOf(uebung_id) > -1);
+        console.log(gehoerteUebungen.indexOf(uebung_id) > -1)
+        return (gehoerteUebungen.indexOf(uebung_id) > -1);
     }
 
     return(
