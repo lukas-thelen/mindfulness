f89bc9a10f3613073e89cda0eaef40296af4e8c0
lukas-thelen <lukas.thelen@t-online.de>
2020-12-23 19:18:03 +0100

    Ordnung

--- src/Profil.js
+++ src/Profil.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useContext } from 'react/cjs/react.development';
+import { AppContext } from './context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const ProfilScreen = ()=> {
+    const username = useContext(AppContext).username;
+    const appData = useContext(AppContext).appData
+    const userData = useContext(AppContext).userData
+    const currentUser = useContext(AppContext).currentUser
+    const changeLoggedIn = useContext(AppContext).changeLoggedIn
+    const changeCurrentUser = useContext(AppContext).changeCurrentUser
+    const logout = async () => {
+        try {
+          await AsyncStorage.removeItem('currentUser')
+          changeLoggedIn(false)
+          changeCurrentUser("")
+          console.log("erfolgreich")
+        } catch (e) {
+          console.log(e)
+        }
+      }
+      const test = ()=>{
+        console.log("appData")
+        console.log(appData)
+        console.log("UserData")
+        console.log(userData)
+        console.log("CurrentUser")
+        console.log(currentUser)
+      }
+
     return(
         <View style={styles.container}>
-            <Text>Profil</Text> 
+            <Text>Hallo {username}</Text>
+            <Button title="abmelden" onPress={()=>{logout()}}></Button> 
+            <Button title="test" onPress={()=>{test()}}></Button>
         </View>
     )
 }
