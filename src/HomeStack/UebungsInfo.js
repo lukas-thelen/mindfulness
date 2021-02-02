b63ea46ac97897f472d978266debcc97a7112181
JASIN AZIZI <JASIN.AZIZI@stud.uni-due.de>
2021-02-02 02:24:19 +0100

    kognitive Prozesse

--- src/HomeStack/UebungsInfo.js
+++ src/HomeStack/UebungsInfo.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, TouchableHighlight, Modal, Alert} from 'react-native';
 
 import {kurse} from "../Kursdaten/Kursdatei.js"
 
@@ -8,12 +8,37 @@ export const UebungsInfo =({navigation, route})=>{
 
     const kursIndex = route.params.kursIndex
     const uebungsIndex = route.params.uebungsIndex
+    const [hasBeenClicked, setHasBeenClicked] = useState(false)
 
+    const onPress = () => {
+        setHasBeenClicked(!hasBeenClicked)
+    }
+
+    const renderItem =({item, index})=>{
+        if (hasBeenClicked == false){
+            return (
+                <TouchableOpacity onPress={onPress}><Text>{item.Name}</Text></TouchableOpacity>
+            )
+        } else {
+            return (
+                <TouchableOpacity onPress={onPress}>
+                    <Text>{item.Name}</Text>
+                    <Text>{item.Beschreibung}</Text>
+                </TouchableOpacity>
+            )
+        }
+    }
+    
     return (
         <View style={styles.container}>
             <Text style={{fontSize:18}}>Informationen zur Übung: {kurse[kursIndex].Uebungen[uebungsIndex].Name}</Text>
             <Text></Text>
             <Text style={{textAlign:"justify"}}>{kurse[kursIndex].Uebungen[uebungsIndex].Info}</Text>
+            <FlatList  
+                data={kurse[kursIndex].Uebungen[uebungsIndex].KognitiveProzesse}
+                keyExtractor={item=>item.id}
+                renderItem={renderItem}
+            ></FlatList>
         </View>
     )
 }
@@ -26,6 +51,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         paddingLeft:20,
         paddingRight:20,
-        
     }
   });
