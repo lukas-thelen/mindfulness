4d1a0cbe69c3852655fc4c802c9bdaf7fa4bdda8
smvimein <66899568+smvimein@users.noreply.github.com>
2021-02-03 10:42:39 +0100

    Designänderungen
    Änderung InitBildschirm, Anmelden, Registrieren, Versionauswahl

--- src/InitBildschirm.js
+++ src/InitBildschirm.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, Image } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, Image, ImageBackground } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RadioButtonRN from 'radio-buttons-react-native';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { LinearGradient } from 'expo-linear-gradient';
 
 import {AppContext} from './context.js';
 import { useContext } from 'react';
@@ -10,20 +11,26 @@ export const StartBildschirm = (props) =>{
    
 
     return(
-        <View style={styles.pagewrap, styles.container}>
+      
+      <ImageBackground source={require('../assets/ErsteSeite.png')} style={styles.imagebackground}>
+        <View style={styles.container}>
+        <Text style={{fontSize:40, fontWeight:"bold", color: '#fff', marginBottom: 10}}>Upgrade</Text>
+        <Text style={{fontSize:25, fontWeight:"bold", color: '#fff',marginBottom: 60}}>your mind</Text>
+
         
-        <Text style={{fontSize:30, fontWeight:"bold", color: '#fff'}}>Upgrade</Text>
-        <Text style={{fontSize:20, fontWeight:"bold", color: '#fff'}}>Your Mind</Text>
-
-        <View style={styles.trennlinie}/>
-
         <TouchableOpacity style={styles.containertext1} onPress={() =>props.changeInitPages('Anmelden')}>
-              <Text style={{color: '#fff', fontSize: 20}}>Anmelden</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.containertext1} onPress={() =>props.changeInitPages('Registrieren')}>
-              <Text style={{color: '#fff', fontSize: 20}}>Registrieren</Text>
-        </TouchableOpacity>
-      </View>
+          <Text style={{color: '#fff', fontSize: 25, }}>Anmelden</Text>
+           
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.containertext2} onPress={() =>props.changeInitPages('Registrieren')}>
+                <Text style={{color: '#fff', fontSize: 17,textDecorationLine: "underline"}}>Registrieren</Text>
+          </TouchableOpacity>
+          <Image source={require('../assets/Mädchen(1).png')} style={styles.image}/>
+        </View>
+        </ImageBackground>
+        
+       
+      
       
 
     )
@@ -31,36 +38,34 @@ export const StartBildschirm = (props) =>{
 
 //Styles
 const styles = StyleSheet.create({
-    container: {
+    container:{
       flex: 1,
       width: '100%',
-      backgroundColor: '#0F113A',
       alignItems: 'center',
-      justifyContent: 'center',
-      
+      justifyContent: 'center' ,
     },
     pagewrap:{
       width: '100%',
-      height: '100%'
+      height: '100%',
     },
     radio:{
       width: 200,
       borderWidth: 0,
-      height:30
+      height:30,
     },
     trennlinie:{
       height:1,
       width:"100%",
       backgroundColor:"black",
       marginBottom:10,
-      marginTop:10
+      marginTop:10,
     },
     containertext1: {
       alignItems:'center',
-      width: '40%',
-      height: '4%',
+      width: '55%',
+      height: '5%',
       borderRadius: 100,
-      backgroundColor: '#80DEE4',
+      backgroundColor: '#464982',
       marginTop: 30,
       shadowColor: 'black',
       shadowOpacity: 0.8,
@@ -68,4 +73,24 @@ const styles = StyleSheet.create({
       shadowRadius: 8,
       shadowOffset : { width: 10, height: 5}
     },
+    containertext2: {
+      alignItems:'center',
+      marginTop: 15,
+      
+    },
+    imagebackground: {
+      flex: 1,
+      resizeMode: 'cover',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    image: {
+      position: 'absolute',
+      top: 490,
+      left: 200,
+      width: 170,
+      height: 170
+     
+    },
+  
   });
